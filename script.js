c6f28020fe6c388779000608e4f836c42fa3f822
babykrishnarayaguru <babykrishnarayaguru@gmail.com>
2023-04-26 17:03:15 +0530

    Created analog stopwatch

--- script.js
+++ script.js
@@ -32,3 +32,27 @@ function restart() {
     [seconds, minutes, hours] = [0, 0, 0];
     displayTime.innerHTML = "00:00:00";
 }
+
+let button1 = document.getElementById('button1');
+let button2 = document.getElementById('button2');
+let button3 = document.getElementById('button3');
+let audio1 = new Audio();
+audio1.src = "assets/click.mp3";
+button1.addEventListener('click', function () {
+    audio1.play();
+});
+button2.addEventListener('click', function () {
+    audio1.play();
+});
+button3.addEventListener('click', function () {
+    audio1.play();
+});
+
+
+// add tik tik sound to clock
+
+// let audio2 = new Audio();
+// audio1.src = "assets/tik.mp3";
+// displayTime.addEventListener('click', function () {
+//     audio2.play();
+// });
